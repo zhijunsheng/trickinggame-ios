3de093943b369b2d73c349b80989e220e9f53c02
daobao <lucashongyuanzhang@gmail.com>
2019-11-23 09:17:10 +0800

    we have a baord

---
+++ TrickingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrickingGame
+//
+//  Created by daobao on 2019/11/23.
+//  Copyright © 2019 daobao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
