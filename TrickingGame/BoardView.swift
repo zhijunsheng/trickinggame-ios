15494a2aca9ffcaa72dbad49b168e62dabb3c173
daobao <lucashongyuanzhang@gmail.com>
2019-11-24 09:11:07 +0800

    we added 8 images

---
+++ TrickingGame/BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  TrickingGame
+//
+//  Created by daobao on 2019/11/23.
+//  Copyright © 2019 daobao. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
