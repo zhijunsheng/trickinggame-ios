ca0469170c3ac7410f190beb4a23ee13cc2d0ee9
daobao <lucashongyuanzhang@gmail.com>
2019-11-24 09:13:40 +0800

    we added missing files

--- TrickingGame/BoardView.swift
+++ TrickingGame/BoardView.swift
@@ -1,21 +1,21 @@
-//
-//  BoardView.swift
-//  TrickingGame
-//
-//  Created by daobao on 2019/11/23.
-//  Copyright © 2019 daobao. All rights reserved.
-//
-
 import UIKit
 
 class BoardView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    let rectWidth: CGFloat = 104
+    let rectHeight: CGFloat = 72.8
+    
     override func draw(_ rect: CGRect) {
         // Drawing code
+        for j in 0..<10 {
+            for i in 0..<7 {
+                    let piece1 = UIBezierPath()
+                    piece1.move(to: CGPoint(x: 0, y: 0 + CGFloat(j) * rectHeight))
+                    piece1.addLine(to: CGPoint(x: rectWidth + CGFloat(i) * rectWidth, y: 0 + CGFloat(j) * rectHeight))
+                    piece1.addLine(to: CGPoint(x: rectWidth + CGFloat(i) * rectWidth, y: rectHeight + CGFloat(j) * rectHeight))
+                    piece1.addLine(to: CGPoint(x: 0, y: rectHeight * (CGFloat(j) + 1)))
+                    piece1.close()
+                    piece1.stroke()
+            }
+        }
     }
-    */
-
 }
