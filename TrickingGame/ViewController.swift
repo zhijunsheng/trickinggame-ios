215b615e85c679baab991940084ec97275b4dd6d
daobao <lucashongyuanzhang@gmail.com>
2019-12-10 18:59:57 +0800

    we have the right timer

--- TrickingGame/ViewController.swift
+++ TrickingGame/ViewController.swift
@@ -21,26 +21,39 @@ class ViewController: UIViewController, TrickingDelegate {
         trickingView.trickingDelegate = self
         brain.reset()
         trickingView.trickingPlaneShadow = brain.trickingPlane
+        
     }
+    
+    /**
+     closure: a function without a name
+     */
 
     @IBAction func beginTimer(_ sender: Any) {
         timerLabel.text = "02:00"
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            print("next second")
-            self.timeHelper -= 1
-            let s = self.timeHelper % 60
-            let min = Int(self.timeHelper / 60)
-            self.timerLabel.text = "0\(min):\(s)"
-            
-            if self.timeHelper == 0 {
-                timer.invalidate()
-                self.endedSound.play()
-                self.trickingView.isUserInteractionEnabled = false
-            }
+            self.abcd(timer: timer)
         }
     }
     
-   
+    /**
+     line 28
+     */
+    func abcd(timer: Timer) {
+        print("next second")
+        self.timeHelper -= 1
+        let s = self.timeHelper % 60
+        let min = Int(self.timeHelper / 60)
+        self.timerLabel.text = "0\(min):\(s)"
+        
+        if self.timeHelper == 0 {
+            timer.invalidate()
+            self.endedSound.play()
+            self.trickingView.isUserInteractionEnabled = false
+            self.timeHelper = 120
+            timerLabel.text = "02:00"
+
+        }
+    }
     
     func movePiece(fx: Int, fy: Int, tx: Int, ty: Int) {
         brain.movePiece(fx: fx, fy: fy, tx: tx, ty: ty)
